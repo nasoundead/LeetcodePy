24d35cdcc0a02bf192c51506a2b70a4cd59d0893
nasoundead <nasoundead@163.com>
2022-09-25 21:58:50 +0800

    refresh

--- Problems/0001-Two-Sum.py
+++ Problems/0001-Two-Sum.py
@@ -1,4 +1,4 @@
-'''
+"""
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -8,9 +8,10 @@ Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
-'''
+"""
 
 from typing import List
+
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
@@ -23,7 +24,17 @@ class Solution:
         return []
 
 
+def test():
+    s = Solution()
+    ans = s.twoSum([1, 2, 3], 3)
+    print(ans)
 
-if __name__ == '__main__':
-    nums = [2, 7, 11, 15]
-    print(Solution().twoSum(nums, 9))
+
+def main():
+    s = Solution()
+    ans = s.twoSum([2, 7, 11, 15], 9)
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Problems/hello.py
@@ -0,0 +1,9 @@
+def main():
+    print("hello world")
+
+
+for i in range(10):
+    print(f'{i}st print statement')
+
+if __name__ == "__main__":
+    main()
